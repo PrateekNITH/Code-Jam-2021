05dcfba564cb6b770b35861aaf901fe10e685d9e
Prateek Srivastava <70479905+PrateekNITH@users.noreply.github.com>
2021-03-28 12:10:45 +0530

    Update Moons_And_Umbrellas.cpp

--- Moons_And_Umbrellas.cpp
+++ Moons_And_Umbrellas.cpp
@@ -1,1 +1,55 @@
-//Will complete after the end of qualification round
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int t,i;
+    cin>>t;
+    for(i=1;i<=t;i++)
+    {
+        int x,y,cost=0,j=0,k=0;
+        bool flag=0;
+        cin>>x>>y;
+        string arr;
+        cin>>arr;
+        char a[1000];
+        
+        for(j=0;j<arr.length();j++)
+        {
+            if(arr[j]!='?')
+            {
+                flag=1;
+                a[k]=arr[j];
+                k++;
+            }
+        }
+        
+        for(j=1;j<k;j++)
+        {
+            if(a[j]=='J')
+            {
+                if(a[j-1]=='C')
+                cost=cost+x;
+            }
+            
+            if(a[j]=='C')
+            {
+                if(a[j-1]=='J')
+                cost=cost+y;
+            }
+        }
+        
+        if(flag==1)
+        {
+            cout<<"Case #"<<i<<": "<<cost<<"\n";
+        }
+        else
+        {
+            cout<<"Case #"<<i<<": 0\n";
+        }
+        
+    }
+    
+    return 0;
+}
